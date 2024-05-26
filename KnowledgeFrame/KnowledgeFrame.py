ee61ee59d0accb9357aac61f9b4131b7777ba527
lolomarka <lolomarka@gmail.com>
2024-05-26 10:01:06 +0500

    init KnowledgeFrames class

---
+++ KnowledgeFrame/KnowledgeFrame.py
@@ -0,0 +1,25 @@
+class KnowledgeFrames:
+    def __init__(self):
+        self.data = {}
+
+    def add_entry(self, key, value : Frame):
+        self.data[key] = value
+
+    def get_entry(self,key):
+        return self.data.get(key, self.key_not_found)
+    
+    def delete_entry(self, key):
+        if key in self.data:
+            del self.data[key]
+        else:
+            self.key_not_found()
+        
+    def list_entries(self):
+        return self.data.keys()
+    
+    def search_entries(self, search_term):
+        results = {key: value for key, value in self.data.items() if search_term.lower() in key.lower()}
+        return results
+    
+    def key_not_found(self, key):
+        raise KeyError(key)
