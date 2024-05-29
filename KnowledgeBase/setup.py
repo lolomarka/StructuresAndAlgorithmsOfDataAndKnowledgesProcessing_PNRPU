d8b6b2493f48741006dceeadcbd2bf393a1ec7e3
lolomarka <lolomarka@gmail.com>
2024-05-29 20:53:53 +0500

    change package folders name, create test txt

--- KnowledgeBase/setup.py
+++ KnowledgeBase/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='KnowledgeFrames',
+    name='KnowledgeBase',
     version='0.1.0',
     author='Mark Sheretov',
     author_email='mark.sheretov@gmail.com',
-    description='Небольшая библиотека для представления KnowledgeFrame',
+    description='Небольшая библиотека для представления базы знаний',
     long_description=open('README.md').read(),
     url='https://github.com/lolomarka/StructuresAndAlgorithmsOfDataAndKnowledgesProcessing_PNRPU/tree/main/KnowledgeFrames',
     packages=find_packages(),
--- KnowledgeFrames/Knowledge_Frames/__init__.py
+++
@@ -1,2 +0,0 @@
-from .Frame import Frame
-from .Knowledge_Frames import KnowledgeFrames
